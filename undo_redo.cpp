68e9f756e37bb369fa3b2587a667e2f7d4c70be8
nguyenkimngan999 <nguyenkimngan0917503893@gmail.com>
2025-04-10 15:54:41 +0700

    Update undo_redo.cpp

--- undo_redo.cpp
+++ undo_redo.cpp
@@ -1,19 +1,26 @@
 #include "undo_redo.h"
 #include "game.h"
+#include "config.h"
 #include <iostream>
 
 void undo_redo::undo(int b[size][size]) {
+    if(undostack.empty()) {
+        std::cout << "Khong the undo!\n";
+    }
     redostack.push(b);
     undostack.pop(b);
 }
 
 void undo_redo::redo(int b[size][size]) {
+    if(redostack.empty()) {
+        std::cout << "Khong the redo!\n";
+    }
     undostack.push(b);
     redostack.pop(b);
 }
 
 //Lưu trạng thái trước khi di chuyển
-void undo_redo::saveSate(int b[size][size]) {
+void undo_redo::saveState(int b[size][size]) {
     undostack.push(b);
     redostack.clear();
 }
