5d4a0d97cf7ec3fbdfa367c0c0153ec254cbc408
nguyenkimngan999 <nguyenkimngan0917503893@gmail.com>
2025-04-17 16:37:03 +0700

    Update undo_redo.cpp

--- undo_redo.cpp
+++ undo_redo.cpp
@@ -6,24 +6,24 @@
 Stack undostack;
 Stack redostack;
 
-void undo(int b[MAX][MAX]) {
+void undo(int b[MAX][MAX], long long& score) {
     if(undostack.empty()) {
         std::cout << "Khong the undo!\n";
     }
-    redostack.push(b);
-    undostack.pop(b);
+    redostack.push(b, score);
+    undostack.pop(b, score);
 }
 
-void redo(int b[MAX][MAX]) {
+void redo(int b[MAX][MAX], long long& score) {
     if(redostack.empty()) {
         std::cout << "Khong the redo!\n";
     }
-    undostack.push(b);
-    redostack.pop(b);
+    undostack.push(b, score);
+    redostack.pop(b, score);
 }
 
 //Lưu trạng thái trước khi di chuyển
-void saveState(int b[MAX][MAX]) {
-    undostack.push(b);
+void saveState(int b[MAX][MAX], long long score) {
+    undostack.push(b, score);
     redostack.clear();
 }
