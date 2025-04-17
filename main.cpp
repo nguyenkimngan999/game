7e3322c573ea9481e2ff2da9b0bbe937dd09f36e
nguyenkimngan999 <nguyenkimngan0917503893@gmail.com>
2025-04-17 16:36:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "board.h"
 #include "undo_redo.h"
 #include "config.h"
+#include "reset_game.h"
 
 #include <iostream>
 #include <cstdlib>
@@ -10,6 +11,13 @@
 int main() {
     std::srand(time(0));
     std::cout << "Welcome to 2048 Game!\n";
+    std::cout << "Ban co muon New Game khong: \n";
+    std::cout << "Nhap 'Co' hoac 'Khong' : ";
+    std::string s;
+    std::cin >> s;
+    if(s == "Co") {
+        reset(undostack, redostack, score);
+    }
     std::cout << "Vui long chon kich thuoc mang: ";
     std::cin >> sizet;
     // Khởi tạo bàn cờ ban đầu
@@ -20,12 +28,12 @@ int main() {
     board();
     // Vòng lặp chính của game
     while (!isgameover()) {
-        hand(check);
+        hand(check, score);
         if(check == true) {
             break;
         }
         board(); // Cập nhật bảng sau mỗi lượt đi
-        std::cout << "So diem ban da choi: " << score << std::endl;
+        std::cout << "So diem hien tai cua ban: " << score << std::endl;
     }
 
     std::cout << "Thanks for playing.\n";
